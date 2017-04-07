fd620808bd3b83ed9de35e0636b53d58cdccb983
Kendry <kendryfbo@gmail.com>
2017-04-07 15:57:55 -0300

    Se agregaron etiquetas para tipos de cheques

--- resources/views/pdf/chequespdf.blade.php
+++ resources/views/pdf/chequespdf.blade.php
@@ -23,18 +23,18 @@
             }
             .monto-1 {
                 position:relative;
-                top: -30px;
-                left: 500px;
+                top: -32px;
+                left: 515px;
             }
             .fecha-1 {
                 position: relative;
-                left: 580px;
+                left: 590px;
                 top: -17px;
             }
             .monto-2 {
                 position: relative;
                 left: 30px;
-                top: -25px;
+                top: -24px;
             }
             .nombre-1 {
                 position: relative;
@@ -43,27 +43,40 @@
             }
             .monto-letras {
                 position:relative;
-                top: -45px;
+                top: -100px;
                 left: 270px;
                 width: 350px;
             }
             .fecha-2 {
                 position: relative;
                 left: 30px;
-                top: -70px;
+                top: -72px;
             }
             .nombre-2 {
                 position: relative;
                 width:150px;
                 height:15px;
                 left:30px;
-                top: -60px;
+                top: -62px;
                 overflow: hidden;
             }
             .descripcion {
                 position: relative;
-                left:30px;
+                left:32px;
                 top: -55px;
+            }
+            .portador {
+                position: relative;
+                left: 715px;
+                top: -90px;
+            }
+            .orden-de {
+                position: relative;
+                left: 220px;
+                top: -75px;
+            }
+            .cruzado {
+
             }
         </style>
     </head>
@@ -72,9 +85,11 @@
         @foreach ($cheques as $cheque)
             <div class="contenedor">
                 <p class="monto-1">{{$cheque['monto']}}</p>
-                <pre class="fecha-1">{{$cheque['dia']}}    {{$cheque['mes']}}    {{$cheque['año']}}</pre>
+                <pre class="fecha-1">{{$cheque['dia']}}          {{$cheque['mes']}}          {{$cheque['año']}}</pre>
                 <p class="monto-2">{{$cheque['monto']}}</p>
                 <p class="nombre-1">{{$cheque['nombre']}}</p>
+                <p class="orden-de" hidden>------------</p>
+                <p class="portador">------------</p>
                 <p class="monto-letras">{{$cheque['montoLetras']}}</p>
                 <p class="fecha-2">{{$cheque['fecha']}}</p>
                 <p class="nombre-2">{{$cheque['nombre']}}</p>
